c5def1906b27294b24c39c85f90a4bbdce82ac77
Saverio Proto <saverio.proto@switch.ch>
2015-08-13 09:49:03 +0200

    Still errors after calling sudo foreman-installer but most stuff works

--- Vagrantfile
+++ Vagrantfile
@@ -1,12 +1,13 @@
 Vagrant.configure("2") do |config|
-  config.vm.define "web" do |web|
-    web.vm.box = "ubuntu/trusty64"
-  end
+#  config.vm.define "web" do |web|
+#    web.vm.box = "ubuntu/trusty64"
+#  end
   config.vm.define "puppetmaster" do |web|
     web.vm.box = "ubuntu/trusty64"
     web.vm.provision :shell, path: "bootstrap.sh"
     web.vm.network :forwarded_port, host: 8000, guest: 80
-    web.vm.network :forwarded_port, host: 8443, guest: 8443
+    web.vm.network :forwarded_port, host: 8443, guest: 443
+    web.vm.network :forwarded_port, host: 9443, guest: 8443
     web.vm.provider "virtualbox" do |v|
       v.memory = 1024
       v.cpus = 2
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y vim htop
+
+apt-get -y install ca-certificates
+#wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
+#dpkg -i puppetlabs-release-precise.deb
+
+#echo "deb http://deb.theforeman.org/ precise 1.8" > /etc/apt/sources.list.d/foreman.list
+#echo "deb http://deb.theforeman.org/ plugins 1.8" >> /etc/apt/sources.list.d/foreman.list
+#wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+#apt-get update && apt-get -y install foreman-installer
+
+echo "deb http://deb.theforeman.org/ trusty stable" > /etc/apt/sources.list.d/foreman.list
+wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+apt-get update && apt-get install -y foreman-installer
+ip=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+echo "$ip foreman.example.com foreman" >> /etc/hosts
+echo "foreman.example.com" > /etc/hostname
+hostname --file /etc/hostname
+#rm -rf /var/lib/puppet/ssl
+#foreman-installer
+
