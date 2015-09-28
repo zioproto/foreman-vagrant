006ef2f8bc7296449ff0b41d18d452649557a314
Saverio Proto <saverio.proto@switch.ch>
2015-09-28 17:19:23 +0200

    Install foreman_templates plugin by default

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,7 @@ apt-get -y install ca-certificates
 #apt-get update && apt-get -y install foreman-installer
 
 echo "deb http://deb.theforeman.org/ trusty stable" > /etc/apt/sources.list.d/foreman.list
+echo "deb http://deb.theforeman.org/ plugins 1.8" >> /etc/apt/sources.list.d/foreman.list
 wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 apt-get update && apt-get install -y foreman-installer
 ip=`ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
@@ -21,4 +22,4 @@ hostname --file /etc/hostname
 rm -rf /var/lib/puppet/ssl
 /usr/bin/puppet cert --generate foreman.example.com
 foreman-installer
-
+apt-get install -y ruby-foreman-templates
